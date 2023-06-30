46b2764846a5281e3eeccaf495cda812e6eef1eb
jcdev92 <jcdeveloper92@gmail.com>
2023-06-30 16:09:32 -0400

    restructuring components files

---
+++ portfolio/src/components/dashboard/SocialMedia/SocialMedia.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const SocialMedia = () => {
+  return <div className="
+    flex
+    flex-col
+    w-full
+    bg-transparent
+    h-screen
+    items-center
+    justify-center
+    text-white
+    text-4xl
+  ">
+    <h1>Social Media</h1>
+  </div>;
+};
