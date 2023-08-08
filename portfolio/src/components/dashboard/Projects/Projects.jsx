b130bdb672d2361b71ae7d070de70665309d9210
jcdev92 <jcdeveloper92@gmail.com>
2023-08-07 20:05:39 -0400

    styling the dashboard table

--- portfolio/src/components/dashboard/Projects/Projects.jsx
+++ portfolio/src/components/dashboard/Projects/Projects.jsx
@@ -1,12 +1,12 @@
+import { ProjectsTable } from './ProjectsTable';
 import useStatusStore from "../../../store/useStatusStore";
 
 export const Projects = () => {
   useStatusStore.getState().setSuccess(null);
   useStatusStore.getState().setError(null);
   return (
-    <div
-      className="flex flex-col w-full bg-transparent h-screen items-center justify-center text-white text-4xl">
-      <h1>Projects</h1>
+    <div className="flex flex-col w-full bg-transparent h-screen items-center justify-center text-white text-4xl">
+        <ProjectsTable />
     </div>
   );
 };
