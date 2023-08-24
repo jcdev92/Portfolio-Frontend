f2df11dd0240c1046f98b21a0fa30944c02745ee
jcdev92 <jcdeveloper92@gmail.com>
2023-08-24 17:46:21 -0400

    skillsProject container styled

--- portfolio/src/components/dashboard/Projects/SkillsContainer.jsx
+++ portfolio/src/components/dashboard/Projects/SkillsContainer.jsx
@@ -2,14 +2,18 @@
 
 const SkillsContainer = ({ project }) => {
   const { ProjectsSkills } = project;
-  
+
   return (
     <div className="relative z-0 w-full mb-6 group">
-      <div className="border overflow-y-auto rounded-md w-full flex justify-around py-4">
+      <h5 className="font-bebas p-4">Project Skills</h5>
+      <div className="rounded-md w-full flex justify-around p-4 flex-wrap gap-4">
         {ProjectsSkills.map(({ Skill }) => (
-          <div key={Skill.id} className="flex gap-2">
-            <span className="text-white text-sm">{Skill.title}</span>
-            <img src={Skill.icon} alt={Skill.title} height={20} width={20} />
+          <div key={Skill.id} className="p-4 flex shadow-md gap-4 rounded-md">
+            <div className="flex gap-2">
+              <span className="text-white text-sm">{Skill.title}</span>
+              <img src={Skill.icon} alt={Skill.title} height={20} width={20} />
+            </div>
+            <button className="text-white text-sm">X</button>
           </div>
         ))}
       </div>
