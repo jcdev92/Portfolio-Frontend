f17b3dffc75c0cc23fe826a19a80152f20e94bb3
jcdev92 <jcdeveloper92@gmail.com>
2023-08-24 23:31:29 -0400

    unnecessary checkbox removed from all the tables, at least while it wont used

--- portfolio/src/components/dashboard/Projects/ProjectsTable.jsx
+++ portfolio/src/components/dashboard/Projects/ProjectsTable.jsx
@@ -43,18 +43,6 @@ export const ProjectsTable = () => {
         <table className="w-full sm:rounded-lg text-sm text-left text-white">
           <thead className="text-xs text-white uppercase">
             <tr className="sticky z-10 top-0 backdrop-blur-sm bg-white/10">
-              <th scope="col" className="p-4">
-                <div className="flex items-center">
-                  <input
-                    id="checkbox-all-search"
-                    type="checkbox"
-                    className="w-4 h-4 text-sky-600 bg-white border-gray-300 rounded"
-                  />
-                  <label htmlFor="checkbox-all-search" className="sr-only">
-                    checkbox
-                  </label>
-                </div>
-              </th>
               <th scope="col" className="px-6 py-3">
                 <h1 className="font-bebas font-light text-lg">title</h1>
               </th>
@@ -75,21 +63,6 @@ export const ProjectsTable = () => {
             ) : (
               data?.map(({ id, title, description, url }) => (
                 <tr key={id} className="w-auto h-auto">
-                  <td className="w-4 p-4">
-                    <div className="flex items-center">
-                      <input
-                        id="checkbox-table-search-2"
-                        type="checkbox"
-                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                      />
-                      <label
-                        htmlFor="checkbox-table-search-2"
-                        className="sr-only"
-                      >
-                        checkbox
-                      </label>
-                    </div>
-                  </td>
                   <th
                     scope="row"
                     className="px-6 py-4 font-medium whitespace-nowrap"
