17862294a2b5c92f2e794c6809bbed69d3c201a3
Jesus Clemente <jcdeveloper92@gmail.com>
2023-09-11 23:15:12 -0400

    fixed - login loader added

--- portfolio/src/components/Login.jsx
+++ portfolio/src/components/Login.jsx
@@ -2,25 +2,29 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
-import { Button, TextInput } from "flowbite-react";
+import { Button, Spinner, TextInput } from "flowbite-react";
 import axios from "axios";
 import { ErrorAlert } from "./dashboard/Alerts/ErrorAlert";
 import { FaM } from "react-icons/fa6";
 import { AiFillLock } from "react-icons/ai";
+import { Loading } from "../components/Loading";
 
 export const Login = () => {
   const { register, handleSubmit } = useForm();
   const navigate = useNavigate();
   const [err, setErr] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const onSubmit = (data) => {
     const { email, password } = data;
+    setLoading(true);
     axios
       .post("https://portfolio-backend-3jrx-dev.fl0.io/api/v1/auth/login", {
         email,
         password,
       })
       .then((res) => {
+        setLoading(false);
         localStorage.setItem("token", res.data.token);
         navigate("/dashboard");
       })
@@ -33,53 +37,44 @@ export const Login = () => {
   };
 
   return (
-    <div
-      className="
-    flex
-    flex-col
-    justify-center
-    items-center
-    h-screen
-    bg-gradient-to-r
-    from-blue-800
-    to-indigo-950
-    text-white
-    font-mono
-    "
-    >
-      <div className="flex flex-col items-center gap-2 mb-8">
-        <h1 className="text-4xl font-bebas drop-shadow-lg">JC DEV PANEL</h1>
-        <h1 className="text-4xl font-bebas drop-shadow-lg">LOGIN</h1>
-      </div>
-      <form
-        className="flex max-w-md flex-col gap-4 lg:w-96 font-sans"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <div className="flex flex-row items-center gap-2">
-          <FaM />
-          <TextInput
-            id="email1"
-            placeholder="email"
-            required
-            type="email"
-            className="w-full"
-            {...register("email", { required: true })}
-          />
-        </div>
-        <div className="flex flex-row items-center gap-2">
-          <AiFillLock />
-          <TextInput
-            id="password"
-            required
-            type="password"
-            placeholder="Password"
-            className="w-full"
-            {...register("password", { required: true })}
-          />
-        </div>
-        <Button type="submit">Login</Button>
-        {err && <ErrorAlert error={err} />}
-      </form>
+    <div className=" flex flex-col justify-center items-center h-screen bg-gradient-to-r from-blue-800 to-indigo-950 text-white font-mono">
+      {loading ? (
+        <Loading />
+      ) : (
+        <form
+          className="flex max-w-md flex-col gap-4 lg:w-96 font-sans"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="flex flex-col items-center gap-2 mb-8">
+            <h1 className="text-4xl font-bebas drop-shadow-lg">JC DEV PANEL</h1>
+            <h1 className="text-4xl font-bebas drop-shadow-lg">LOGIN</h1>
+          </div>
+          <div className="flex flex-row items-center gap-2">
+            <FaM />
+            <TextInput
+              id="email1"
+              placeholder="email"
+              required
+              type="email"
+              className="w-full"
+              {...register("email", { required: true })}
+            />
+          </div>
+          <div className="flex flex-row items-center gap-2">
+            <AiFillLock />
+            <TextInput
+              id="password"
+              required
+              type="password"
+              placeholder="Password"
+              className="w-full"
+              {...register("password", { required: true })}
+            />
+          </div>
+          <Button type="submit">Login</Button>
+          {err && <ErrorAlert error={err} />}
+        </form>
+      )}
     </div>
   );
 };
