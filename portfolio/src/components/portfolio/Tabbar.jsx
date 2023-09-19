ee4471cd24be5ac124330b9a8c43ed37368a51fb
Jesus Clemente <jcdeveloper92@gmail.com>
2023-09-19 19:10:10 -0400

    wroking on portfolio layout - forgive add tab bar component file

---
+++ portfolio/src/components/portfolio/Tabbar.jsx
@@ -0,0 +1,11 @@
+const Tabbar = () => {
+  return (
+    <div className="flex items-start h-full w-full bg-odp-bg">
+      <div className="border w-1/12 h-full">
+        <button></button>
+      </div>
+    </div>
+  );
+};
+
+export default Tabbar;
