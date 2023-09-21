111be8d1c544d0c9b66f999486f24d0662a13d49
Jesus Clemente <jcdeveloper92@gmail.com>
2023-09-21 03:11:17 -0400

    fetchin profile data and setting the stale time

---
+++ portfolio/src/components/portfolio/Layout/NumbersCol.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+
+const NumbersCol = () => {
+  // Crear una variable de estado para guardar el alto de la pantalla
+  const [height, setHeight] = useState(window.innerHeight*0.97);
+
+  // Crear una función para manejar el evento resize
+  function handleResize() {
+    // Actualizar el valor del alto de la pantalla
+    setHeight(window.innerHeight*0.93);
+  }
+
+  // Usar el hook useEffect para agregar y eliminar el listener del evento resize
+  useEffect(() => {
+    // Agregar el listener cuando el componente se monte
+    window.addEventListener("resize", handleResize);
+
+    // Devolver una función de limpieza que elimine el listener cuando el componente se desmonte
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  let numSize = Math.floor(height/26);
+
+
+  const numbers = [];
+  for (let i = 0; i < numSize; i++) {
+    numbers.push(i + 1);
+  }
+  return (
+    <div className="flex flex-col pl-2">
+      {numbers.map((number) => (
+        <div key={number}>{number}</div>
+      ))}
+    </div>
+  );
+};
+
+export default NumbersCol;
---
+++ portfolio/src/components/portfolio/Layout/TabBar.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable react/prop-types */
+const TabBar = ({ sections, setWichSelected, whichSelected }) => {
+  // copy the tags of each section into an array
+  const arrayTags = [...sections.map((section) => section.tag)];
+
+  // compare if the tag passed from the respective section, are in the array tags, if it true, then it will be selected to the state isSelected.
+  const compareTags = (sectionTag) => {
+    arrayTags.includes(sectionTag) && setWichSelected(sectionTag);
+  };
+
+  return (
+    <div className="flex w-full h-full">
+      {sections.map((section, index) => (
+        <button
+          onClick={() => compareTags(section.tag)}
+          key={index} // Asegúrate de agregar una clave única para cada elemento en el array
+          className={
+            whichSelected === section.tag
+              ? "bg-odp-foreground border-b flex items-center gap-2 h-full px-4 hover:bg-odp-text-light"
+              : "bg-odp-bg flex items-center gap-2 h-full px-4 hover:bg-odp-text-light"
+          }
+        >
+          {
+            // render the icons here
+            section[section.tag]
+          }
+          <span className="text-white">{section.fileName}</span>
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default TabBar;
