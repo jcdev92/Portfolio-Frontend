86f8fae47433d6327dbd3b8d7bef49e256961b9c
Jesus Clemente <jcdeveloper92@gmail.com>
2023-09-21 10:58:25 -0400

    working on intro styles

--- portfolio/src/components/portfolio/Sections/Intro.jsx
+++ portfolio/src/components/portfolio/Sections/Intro.jsx
@@ -1,5 +1,30 @@
+import useProfileStore from "../../../store/useProfileStore";
 export const Intro = () => {
+  const profile = useProfileStore((state) => state.profile);
+  const {
+    firstName,
+    lastName,
+    aboutMe,
+    country,
+    profileImg,
+    jobTitle,
+  } = profile;
   return (
-    <div>Intro</div>
-  )
-}
+    <div className="flex h-full text-odp-text font-mono">
+      <div className="border">
+        <img
+          className="w-[10%]"
+          style={{ borderRadius: "50%" }}
+          src={profileImg}
+          alt="profileImg"
+        />
+        <h1>
+          {firstName} {lastName}
+        </h1>
+        <p>{jobTitle}</p>
+        <p>{country}</p>
+      </div>
+      <p>{aboutMe}</p>
+    </div>
+  );
+};
