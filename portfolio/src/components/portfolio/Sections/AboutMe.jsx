a3f1d1fd6c626fc24e0f8e260b14eb96d038481a
Jesus Clemente <jcdeveloper92@gmail.com>
2023-09-26 13:57:22 -0400

    AboutMe.jsx styling and fixing a useProfile axios intercepetor

--- portfolio/src/components/portfolio/Sections/AboutMe.jsx
+++ portfolio/src/components/portfolio/Sections/AboutMe.jsx
@@ -1,5 +1,14 @@
+import useProfileStore from "../../../store/useProfileStore";
 export const AboutMe = () => {
+  const profile = useProfileStore((state) => state.profile);
   return (
-    <div>About Me</div>
-  )
-}
+    <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8 h-full p-2">
+      <div className="h-fit rounded-lg p-2">
+        <img src={profile?.bioImage} alt="bioImage" />
+      </div>
+      <div className="h-fit rounded-lg lg:col-span-2 p-2">
+        <p>{profile?.biography}</p>
+      </div>
+    </div>
+  );
+};
