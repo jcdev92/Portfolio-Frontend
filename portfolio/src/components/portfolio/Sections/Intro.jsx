505b3de16610dc96ea1f1d946db8e610c13f80ae
Jesus Clemente <jcdeveloper92@gmail.com>
2023-09-29 15:03:22 -0400

    back to intro component original syle

--- portfolio/src/components/portfolio/Sections/Intro.jsx
+++ portfolio/src/components/portfolio/Sections/Intro.jsx
@@ -26,45 +26,43 @@ export const Intro = () => {
       <div className="loader2"></div>
     </div>
   ) : isSuccess ? (
-    <div className="flex flex-col h-full text-odp-text font-mono border">
-      <div className="flex flex-col h-[50%] w-full border items-center justify-center gap-4">
-        <div className="flex items-center w-full h-full border">
-          <div className="flex w-1/3 h-5/6 p-2">
+    <div className="flex h-full w-full text-odp-text font-mono">
+      <div className="flex flex-col w-[50%] items-center justify-center gap-4">
+        <div className="flex gap-4 items-center justify-center w-full h-2/6">
+          <div className="relative flex w-1/4 h-5/6 justify-center items-center p-2 hover:scale-90 hover:rotateX-180 shadow-md hover:shadow-none tansition-all ease-in-out duration-200 hover:cursor-pointer shadow-gray-700 rounded-full">
+            <img
+              className="absolute border-4 border-double rounded-full z-10 hover:z-0 h-full"
+              src={profile?.profileImg}
+              alt="profileImg"
+            />
             <div
-              className="relative h-full w-full border items-center justify-center"
+              className="absolute flex border-4 border-double h-full w-full items-center justify-center rounded-full bg-red-600 text-white z-0 hover:z-10"
               href="#"
             >
-              <img
-                className="absolute border-4 border-double rounded-full z-10 hover:z-0 h-3/5 w-2/6 hover:scale-90 hover:rotateX-180 hover:shadow-none tansition-all ease-in-out duration-300 hover:cursor-pointer shadow-gray-700 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-                src={profile?.profileImg}
-                alt="profileImg"
-              />
-              <span className="absolute flex border-4 border-double rounded-full bg-red-600 text-white z-0 hover:z-10 uppercase w-2/6 h-3/5 items-center justify-center hover:scale-90 hover:cursor-pointer top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                resume
-              </span>
+              <span className="text-2xl uppercase">resume</span>
             </div>
           </div>
-          <div className="flex h-full w-2/3 text-2xl items-center justify-center border flex-col p-2 text-yellow-300">
+          <div className="flex h-full text-2xl justify-center flex-col p-2 text-yellow-300">
             <h1>{profile?.firstName}</h1>
             <h1>{profile?.lastName}</h1>
-            <div className="flex flex-col justify-center items-center p-2 text-blue-500 border">
-              <Typewriter
-                options={{
-                  strings: [
-                    profile?.jobTitle,
-                    "Front-End Developer",
-                    "Back-End Developer",
-                  ],
-                  autoStart: true,
-                  loop: true,
-                }}
-              />
-              <p className="text-red-500">{profile?.country}</p>
-            </div>
           </div>
         </div>
+        <div className="flex flex-col justify-center items-center p-2 text-blue-500">
+          <Typewriter
+            options={{
+              strings: [
+                profile?.jobTitle,
+                "Front-End Developer",
+                "Back-End Developer",
+              ],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+          <p className="text-red-500">{profile?.country}</p>
+        </div>
       </div>
-      <div className="flex h-[50%] items-center justify-center">
+      <div className="flex w-[50%] items-center justify-center">
         <span className="flex w-2/3">
           <Typewriter
             options={{
