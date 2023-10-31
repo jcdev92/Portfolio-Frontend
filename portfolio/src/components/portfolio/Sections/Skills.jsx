aa861f964acf3deb2888192c184c316de4f44c55
Jesus Clemente <jcdeveloper92@gmail.com>
2023-10-31 11:20:20 -0400

    styling skills section

--- portfolio/src/components/portfolio/Sections/Skills.jsx
+++ portfolio/src/components/portfolio/Sections/Skills.jsx
@@ -6,13 +6,13 @@ export const Skills = () => {
   return (
     <div className="h-full lg:flex lg:justify-center lg:items-center">
       <div className="px-3 md:lg:xl:px-40 bg-opacity-10">
-        <div className="grid xs:grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 lg:grid-rows-3 xl:grid-cols-4 xl:grid-rows-3 group shadow-xl shadow-neutral-100 overflow-y-auto h-fit">
+        <div className="grid xs:grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 lg:grid-rows-3 xl:grid-cols-4 xl:grid-rows-3 group shadow-xl shadow-neutral-100 overflow-y-auto h-fit rounded-md">
           {skills.map(({ id, title, icon }) => (
             <div
               key={id}
-              className="p-10 flex flex-col items-center text-center group hover:bg-slate-50 cursor-pointer"
+              className="p-10 flex flex-col items-center text-center group"
             >
-              <span className="p-5 rounded-full bg-red-500 text-white shadow-sm shadow-red-200">
+              <span className="p-5 rounded-full bg-gray-700 text-white shadow-sm shadow-gray-400 hover:shadow-none hover:cursor-pointer hover:scale-95 transition-all ease-in-out duration-200">
                 <img src={icon} alt="" height="20" width="20"/>
               </span>
               <p className="text-sm font-medium text-slate-700 mt-3">{title}</p>
