0fd3f3e8ff3220d95dd19ae59c98be1e3f64991f
Jesus Clemente <jcdeveloper92@gmail.com>
2024-05-03 19:46:25 -0400

    message service configurated

---
+++ src/hooks/url.js
@@ -0,0 +1,5 @@
+//* API PROD
+export const api = 'https://portfolio-backend-j1tl.onrender.com'
+
+//? API DEV 
+// export const api = 'http://localhost:9000'
---
+++ src/hooks/useBlog.js
@@ -0,0 +1,5 @@
+// import axios from "axios";
+// import { api } from './url';
+
+// production url
+// const url = api
