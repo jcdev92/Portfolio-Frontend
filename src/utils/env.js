0800b32f821a2d11342c6209cd1fee80f504394c
Jesus Clemente <jcdeveloper92@gmail.com>
2024-05-06 13:22:54 -0400

    working on env variables

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 import * as dotenv from 'dotenv'
 
-dotenv.config({ path: '.env' })
+dotenv.config({ path: '.env.local' })
 export const apiDev = process.env.API_URL_DEV;
 export const apiProd = process.env.API_URL_PROD;
 export const nodeEnv = process.env.NODE_ENV;
