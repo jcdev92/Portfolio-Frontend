7d4962b1aa8599daeb5b617d3bd00d5daf3e707c
Jesus Clemente <jcdeveloper92@gmail.com>
2024-05-06 12:54:32 -0400

    changing env variables

--- src/hooks/url.js
+++ src/hooks/url.js
@@ -1,5 +1,4 @@
-//* API PROD
-// export const api = 'https://portfolio-backend-j1tl.onrender.com'
-
-//? API DEV 
-export const api = 'http://localhost:9000'
+/* eslint-disable no-undef */
+export const api = process.env.NODE_ENV === "production"
+  ? process.env.API_URL_PROD
+  : process.env.API_URL_DEV;
